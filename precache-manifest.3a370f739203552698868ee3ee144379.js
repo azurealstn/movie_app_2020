60455d000eef9be0b0d2854ca11bb4be677eca94
azurealstn <azurealstn@naver.com>
2020-10-23 17:08:41 +0900

    Updates

---
+++ precache-manifest.3a370f739203552698868ee3ee144379.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a23c291a86c499e2321793a5b7e7f7",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d339da1c8c85a9f8795a",
+    "url": "/movie_app_2020/static/css/main.c460891e.chunk.css"
+  },
+  {
+    "revision": "ae6bc8df9b8a0b533a7a",
+    "url": "/movie_app_2020/static/js/2.4995e3f2.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/movie_app_2020/static/js/2.4995e3f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d339da1c8c85a9f8795a",
+    "url": "/movie_app_2020/static/js/main.2c069b85.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
